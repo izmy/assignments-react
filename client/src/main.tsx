190779b65d37325fa6d64a41ff5811db9d03f380
Jaroslav Hrách <me@jaroslavhrach.cz>
2024-09-07 16:00:56 +0200

    feat: set env variables

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 
+export const SETTINGS = {
+    environment: import.meta.env.VITE_ENVIRONMENT ?? "development",
+    backendApiUrl: import.meta.env.VITE_BACKEND_API_URL ?? "http://localhost:3000",
+};
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
