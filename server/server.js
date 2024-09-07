7f00925252e520f29084a962c20d86572f6e5582
Jaroslav Hrách <me@jaroslavhrach.cz>
2024-09-07 16:01:15 +0200

    feat: [S1] implement endpoint /items/:id/done

--- server/server.js
+++ server/server.js
@@ -6,10 +6,42 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 server.use(jsonServer.bodyParser);
+
+// custom route for setting item as done with timestamp
+server.patch("/items/:id/done", (req, res, next) => {
+    const db = router.db
+    const id = Number(req.params.id);
+
+    const item = db.get('items').find({ id }).value();
+
+    if (item == null) {
+        res.status(404);
+        res.locals.data = { message: 'Item not found' }
+    } else {
+        const updates = db.get('items')
+        .upsert({ ...item, isDone: true, finishedAt: Date.now() })
+        .write()
+  
+    
+      res.status(200)
+      res.locals.data = updates
+    }
+
+    next()
+})
+
 server.use((req, res, next) => {
     if (req.method === "POST") {
         req.body.createdAt = Date.now();
+        req.body.isDone = false;
     }
+
+    if (req.method === "PATCH") {
+        if (req.body.isDone === false) {
+            req.body.finishedAt = undefined;
+        }
+    }
+    
     next();
 });
 
